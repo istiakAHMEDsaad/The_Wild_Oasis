0c05af51661d841ff06dfd6aca58d11948929c9d
istiakAHMEDsaad <istik2020@gmail.com>
2026-04-16 21:07:31 +0600

    added custom button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Button>Hello World</Button>
+      <Input placeholder='hola' />
+    </>
+  );
 }
 
 export default App;
