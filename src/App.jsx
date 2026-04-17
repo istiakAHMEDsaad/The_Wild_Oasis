1c3c2633f9a0e8b2613fe92d8d72ebe1ea0a1ec4
istiakAHMEDsaad <istik2020@gmail.com>
2026-04-17 12:28:40 +0600

    adding routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,70 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='dashboard'  />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Heading from './ui/Heading';
 import Input from './ui/Input';
+import Row from './ui/Row';
+
+const StyledApp = styled.main`
+  padding: 20px;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Heading as='h1'>The Wild Oasis</Heading>
-      <Button>Print</Button>
-      <Input placeholder='hola' />
+      <StyledApp>
+        <Row type='vertical'>
+          <Row type='horizontal'>
+            <Heading as='h1'>The Wild Oasis</Heading>
+
+            <div>
+              <Heading as='h2'>Check in and out</Heading>
+              <Button
+                variation='primary'
+                size='medium'
+                onClick={() => alert('button clicked')}
+              >
+                Check in
+              </Button>
+              <Button
+                variation='secondary'
+                size='small'
+                onClick={() => alert('button clicked')}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row type='vertical'>
+            <Heading as='h3'>Form</Heading>
+
+            <form>
+              <Input type='number' placeholder='Number of guests' />
+              <Input type='number' placeholder='Number of guests' />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
     </>
   );
 }
 
 export default App;
+*/
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -49,17 +49,13 @@ const variations = {
 };
 
 const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
   border: none;
   border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-500);
-  color: var(--color-brand-50);
   box-shadow: var(--shadow-sm);
   cursor: pointer;
-  &:hover {
-    background-color: var(--color-brand-600);
-  }
+
+  ${(props) => variations[props.variation]}
+  ${(props) => sizes[props.size]}
 `;
 
 export default Button;
