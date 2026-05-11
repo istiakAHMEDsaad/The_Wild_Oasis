15cac826e73ff6f14e3ad927e99c2b2e02d829fc
istiakAHAMMEDsaad <istiakahmedsaad.official@gmail.com>
2026-05-11 17:10:50 +0600

    get current user

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+function ProtectedRoute({ children }) {
+  // 1. Load the authenticated user
+
+  // 2. While loading, show a spinner
+
+  // 3. If there is No authenticated user, redirect to the /login
+
+  // 4. If there is a user, render the app
+
+  return children;
+}
+
+export default ProtectedRoute;
