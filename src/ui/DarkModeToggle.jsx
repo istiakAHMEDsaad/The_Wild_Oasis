63e76078311f3878a85e988d35edde9395910999
istiakAHAMMEDsaad <istiakahmedsaad.official@gmail.com>
2026-05-16 13:15:07 +0600

    added dark mode features

--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -1,10 +1,13 @@
-import { HiOutlineMoon } from "react-icons/hi2";
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
 import ButtonIcon from "./ButtonIcon";
+import { useDarkMode } from "../context/DarkModeContext";
 
 function DarkModeToggle() {
+  const { toggleDarkMode, isDarkMode } = useDarkMode();
+
   return (
-    <ButtonIcon>
-      <HiOutlineMoon />
+    <ButtonIcon onClick={toggleDarkMode}>
+      {isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
     </ButtonIcon>
   );
 }
